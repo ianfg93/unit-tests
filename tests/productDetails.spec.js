698a1087d9399e3b875e67f4657f3acdfc688ed1
Ian Ferreira <ian_ferreira93@hotmail.com>
2022-07-19 18:58:29 -0300

    requisito 4

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -34,9 +34,13 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
     fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se productDetails é uma função.
+    expect (typeof productDetails).toBe('function');
     // Teste se o retorno da função é um array.
+    expect (typeof productDetails()).toBe('array');
     // Teste se o array retornado pela função contém dois itens dentro.
+    expect (Object.keys(productDetails(2)).length).toBe(2);
     // Teste se os dois itens dentro do array retornado pela função são objetos.
+    
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
     // Teste se os dois productIds terminam com 123.
   });
