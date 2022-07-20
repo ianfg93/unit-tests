c0ab22d43bbcbc6cef56385549134369344e2c34
Ian Ferreira <ian_ferreira93@hotmail.com>
2022-07-20 15:39:12 -0300

    requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -1,4 +1,4 @@
-const productDetails = require('../src/productDetails');
+const productDetails = require("../src/productDetails");
 /*
   A função productDetails recebe duas strings que representam nomes de produtos, e retorna um array contendo dois objetos com os detalhes dos respectivos produtos.
 
@@ -29,19 +29,23 @@ const productDetails = require('../src/productDetails');
 
 */
 
-describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
-  it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
+describe("6 - Implemente os casos de teste para a função `productDetails`", () => {
+  it("Verifica se a função `productDetails` tem o comportamento esperado", () => {
+    // fail("Teste vazio!");
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se productDetails é uma função.
-    expect (typeof productDetails).toBe('function');
+    expect(typeof productDetails).toBe("function");
     // Teste se o retorno da função é um array.
-    expect (typeof productDetails()).toBe('array');
+    expect(Array.isArray(productDetails()));
     // Teste se o array retornado pela função contém dois itens dentro.
-    expect (Object.keys(productDetails(2)).length).toBe(2);
+    expect(Object.keys(productDetails()).length).toBe(2);
     // Teste se os dois itens dentro do array retornado pela função são objetos.
-    
+    expect(typeof productDetails()[0]).toBe("object");
+    expect(typeof productDetails()[1]).toBe("object");
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
-    // Teste se os dois productIds terminam com 123.
+    expect(productDetails()[0]).not.toBe(productDetails()[1]);
+    // // Teste se os dois productIds terminam com 123.
+    expect(productDetails()[0].details.productId).toMatch("123");
+    expect(productDetails()[1].details.productId).toMatch("123");
   });
 });
